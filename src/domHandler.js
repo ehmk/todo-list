62e0c502437151e005e1ce58b6e33d1ff3e7e61e
ehmk <ehmk@users.noreply.github.com>
2023-08-11 22:22:51 -0400

    Refactored the show/hide details functionality

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,30 +1,36 @@
 import TodoItem from './createTodoItem';
 import './styles/index.css';
 
+export function createProjectDiv(project) {
+  const div = document.createElement('div');
+  const title = document.createElement('h2');
+
+  title.textContent = project.projectName;
+  div.classList.add('project');
+
+  const divChildren = [title];
+  appendToElement(div, divChildren);
+  return div;
+}
+
 export function createTodoDiv(todoItem) {
   const div = document.createElement('div');
   const title = document.createElement('h4');
   const description = document.createElement('p');
   const dueDate = document.createElement('p');
   const priority = document.createElement('p');
-  const expandBtn = document.createElement('button');
 
   title.textContent = todoItem.title;
   description.textContent = `Description: ${todoItem.description}`;
   dueDate.textContent = `Due Date: ${todoItem.dueDate}`;
   priority.textContent = `Priority: ${todoItem.priority}`;
-  expandBtn.textContent = `Show Details`;
 
   div.classList.add('todo-item');
   description.classList.add('display-hidden');
   priority.classList.add('display-hidden');
-  expandBtn.classList.add('expand-btn');
 
   const taskDetails = [description, priority];
-
-  expandBtn.addEventListener('click', () => {
-    showHideDetails(taskDetails, expandBtn);
-  });
+  const expandBtn = createExpandBtn(taskDetails);
 
   const divChildren = [title, dueDate, description, priority, expandBtn];
   appendToElement(div, divChildren);
@@ -38,28 +44,23 @@ function appendToElement(parent, children) {
   }
 }
 
-export function createProjectDiv(project) {
-  const div = document.createElement('div');
-  const title = document.createElement('h2');
-
-  title.textContent = project.projectName;
-  div.classList.add('project');
-
-  const divChildren = [title];
-  appendToElement(div, divChildren);
-  return div;
-}
-
-function showHideDetails(details, btn) {
-  for (let i = 0; i < details.length; i++) {
-    if (details[i].classList.contains('display-hidden')) {
-      details[i].classList.remove('display-hidden');
-      details[i].classList.add('display-block');
-      btn.textContent = 'Hide Details';
-    } else if (details[i].classList.contains('display-block')) {
-      details[i].classList.remove('display-block');
-      details[i].classList.add('display-hidden');
-      btn.textContent = 'Show Details';
+function createExpandBtn(details) {
+  const btn = document.createElement('button');
+  btn.textContent = `Show Details`;
+  btn.classList.add('expand-btn');
+  function toggleDetails() {
+    for (let i = 0; i < details.length; i++) {
+      if (details[i].classList.contains('display-hidden')) {
+        btn.textContent = 'Hide Details';
+        details[i].classList.remove('display-hidden');
+        details[i].classList.add('display-block');
+      } else if (details[i].classList.contains('display-block')) {
+        btn.textContent = 'Show Details';
+        details[i].classList.remove('display-block');
+        details[i].classList.add('display-hidden');
+      }
     }
   }
+  btn.addEventListener('click', toggleDetails);
+  return btn;
 }
